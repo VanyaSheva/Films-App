07dd346a5b71a636ca803db8b2f8b302cab4e00f
Vanya Shevchuk <aggressor2000@gmail.com>
2020-05-04 17:56:32 +0300

    Updates

---
+++ precache-manifest.3ee9db3ebae07edd8618fcfb147f6cee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f911340be9021fb6002b178d8b257bdd",
+    "url": "/Films-App/index.html"
+  },
+  {
+    "revision": "dd772b5a71029fb05f51",
+    "url": "/Films-App/static/css/cast-page.c47cd5c9.chunk.css"
+  },
+  {
+    "revision": "1eb0e2ce0f454bd37a55",
+    "url": "/Films-App/static/css/film-page.488316c4.chunk.css"
+  },
+  {
+    "revision": "3c22f37b76f486c4e33a",
+    "url": "/Films-App/static/css/home-page.30f2e2a4.chunk.css"
+  },
+  {
+    "revision": "e04725360f85796d500e",
+    "url": "/Films-App/static/css/main.de49d031.chunk.css"
+  },
+  {
+    "revision": "44e6e2589c94dfb2bf7d",
+    "url": "/Films-App/static/css/movies-page.a7c62a62.chunk.css"
+  },
+  {
+    "revision": "ef4a7a8b28b5682b138b",
+    "url": "/Films-App/static/css/reviews-page.1eea3978.chunk.css"
+  },
+  {
+    "revision": "f1f478a0eca317f46b02",
+    "url": "/Films-App/static/js/0.4b3ad949.chunk.js"
+  },
+  {
+    "revision": "9a80b84609cf20af7dad",
+    "url": "/Films-App/static/js/8.d69cc479.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Films-App/static/js/8.d69cc479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd772b5a71029fb05f51",
+    "url": "/Films-App/static/js/cast-page.9f0d372a.chunk.js"
+  },
+  {
+    "revision": "1eb0e2ce0f454bd37a55",
+    "url": "/Films-App/static/js/film-page.26678926.chunk.js"
+  },
+  {
+    "revision": "3c22f37b76f486c4e33a",
+    "url": "/Films-App/static/js/home-page.fbf29430.chunk.js"
+  },
+  {
+    "revision": "e04725360f85796d500e",
+    "url": "/Films-App/static/js/main.334258ff.chunk.js"
+  },
+  {
+    "revision": "44e6e2589c94dfb2bf7d",
+    "url": "/Films-App/static/js/movies-page.916a21a2.chunk.js"
+  },
+  {
+    "revision": "ef4a7a8b28b5682b138b",
+    "url": "/Films-App/static/js/reviews-page.9e0bc52c.chunk.js"
+  },
+  {
+    "revision": "95b81bef86d87d3d08d4",
+    "url": "/Films-App/static/js/runtime-main.a18b2c55.js"
+  }
+]);
